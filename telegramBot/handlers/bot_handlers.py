aa6733ab9cadd3704fa9f8d418b31974ce8b94ff
Wooshey1411 <vad2003.barnaul@gmail.com>
2024-02-27 21:20:12 +0700

    add echo bot

---
+++ telegramBot/handlers/bot_handlers.py
@@ -0,0 +1,20 @@
+from telegram import ForceReply, Update
+from telegram.ext import ContextTypes
+
+__all__ = ['start', 'help_command', 'echo']
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Help!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(update.message.text)
