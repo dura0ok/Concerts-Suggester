d1110b198e77058b25f204867d703872b2b64532
durachok <fenixrnd@mail.ru>
2024-03-22 21:54:32 +0700

    try to add java 0_0

---
+++ concert-suggester-backend/src/main/java/ru/nsu/pipichinka/concertsuggesterbackend/ConcertSuggesterBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.pipichinka.concertsuggesterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertSuggesterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertSuggesterBackendApplication.class, args);
+	}
+
+}
